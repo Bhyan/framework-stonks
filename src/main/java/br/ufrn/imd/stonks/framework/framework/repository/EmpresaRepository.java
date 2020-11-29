db30b64512dc094a84ac0edfea8c5a503d6a6697
LimaLucas96 <l.lima19@hotmail.com>
2020-11-28 23:15:11 -0300

    Adicionado as classes para a parte de repository.

---
+++ src/main/java/br/ufrn/imd/stonks/framework/framework/repository/EmpresaRepository.java
@@ -0,0 +1,8 @@
+package br.ufrn.imd.stonks.framework.framework.repository;
+
+import br.ufrn.imd.stonks.framework.framework.model.Empresa;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpresaRepository extends AbstractRepository<Empresa> {
+}
