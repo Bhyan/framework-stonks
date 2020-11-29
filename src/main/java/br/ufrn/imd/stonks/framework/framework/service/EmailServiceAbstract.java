2f7beb138d9c3e70e6cc525e94d2dcf7bfb82a27
LimaLucas96 <l.lima19@hotmail.com>
2020-11-28 21:40:07 -0300

    Adicionado as classes para a parte de servico.

--- src/main/java/br/ufrn/imd/stonks/framework/framework/service/EmailServiceAbstract.java
+++ src/main/java/br/ufrn/imd/stonks/framework/framework/service/EmailServiceAbstract.java
@@ -1,4 +1,8 @@
 package br.ufrn.imd.stonks.framework.framework.service;
 
+import br.ufrn.imd.stonks.framework.framework.exception.AbstractEntityException;
+
 public abstract class EmailServiceAbstract {
+
+    public abstract String EnviarNotificacao() throws AbstractEntityException;
 }
--- src/main/java/br/ufrn/imd/stonks/framework/framework/service/RelatorioServiceAbstract.java
+++ src/main/java/br/ufrn/imd/stonks/framework/framework/service/RelatorioServiceAbstract.java
@@ -1,6 +1,8 @@
 package br.ufrn.imd.stonks.framework.framework.service;
 
+import br.ufrn.imd.stonks.framework.framework.exception.AbstractEntityException;
+
 public abstract class RelatorioServiceAbstract {
 
-    public abstract void enviarRelatorio();
+    public abstract void enviarRelatorio() throws AbstractEntityException;
 }
